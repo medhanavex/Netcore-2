fad3c33723b8173432401841cca887670b4b3fdf
medha ajoy <medha.ajoy@navexglobal.com>
2021-02-03 09:43:59 +0530

    Commited Final Project

---
+++ Final-Netcore/Final-Netcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Final_Netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Final Project!");
+        }
+    }
+}
